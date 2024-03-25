213f314d9bd2c78141be76b568b8f42f63053ccc
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-03-24 20:56:36 -0400

    Innitial commit

---
+++ src/main/java/com/cristianml/login/Login.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.cristianml.login;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Login {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
